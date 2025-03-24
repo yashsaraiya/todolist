40c943eb0b99b2f7bee41695dd4609a4e0b46f7a
yashsaraiya <166718000+yashsaraiya@users.noreply.github.com>
2025-03-24 04:42:25 -0700

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,7 +4,7 @@ export const Navbar = () => {
   return (
     <nav className='flex justify-between bg-slate-700 text-yellow-400 py-2'>
       <div className="logo">
-        <span className='font-bold text-x1 mx-8 font-rubikVinyl text-3xl'>iTask</span>
+        <span className='font-bold text-x1 mx-8 font-rubikVinyl text-3xl'>Task</span>
       </div>
       <ul className='flex gap-7 mx-9 font-rubikVinyl text-xl'>
         <li className='cursor-pointer hover:font-bold'>home</li>
